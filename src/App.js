f5f94718f22133f791ec431ce8969829fa86f7c0
Laurie Frank <laurie360@hotmail.co.uk>
2019-12-09 14:58:22 +0000

    created initial set up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './App.css';
+import FilmBox from './containers/FilmBox'
 
 function App() {
   return (
-    <h1> Hello World!</h1>
+    <FilmBox />
   );
 }
 
---
+++ src/components/Film.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+class Film extends Component{
+
+  render(){
+    return(
+      <li className="Film">
+        <h4>{this.props.children}</h4>
+        <a href=`{this.props.url}`>{this.props.name}</a>
+      </li>
+    )
+  }
+}
+
+export default Film
